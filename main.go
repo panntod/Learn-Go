9656b1b688cd50cad734ffb073ad6abb0ce810a7
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-14 05:29:16 +0700

    feat: added function convert data type int

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 
 	fmt.Println(greeting(name, age, uri))
 	fmt.Println(calculate(1, 3))
+	convertion(213132)
 }
 
 func greeting(name string, age int, uri string) string {
@@ -25,4 +26,14 @@ func calculate(num1 int, num2 int) string {
 	equal := num1 + num2
 	result := fmt.Sprintf("Result of %d + %d = %d", num1, num2, equal)
 	return result
+}
+
+func convertion(num int) int {
+	var nilai32 int32 = int32(num)
+	fmt.Println("This is a int32", nilai32)
+
+	var nilai64 int64 = int64(num)
+	fmt.Println("This is a int64: ", nilai64)
+
+	return num
 }
